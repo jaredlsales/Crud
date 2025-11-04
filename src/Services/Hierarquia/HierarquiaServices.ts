087ae756bd146eaec4abdc23a58151fcdd76b0aa
Jared Sales <jaredlsales@outlook.com>
2025-11-04 17:33:30 -0300

    04-11

--- src/Services/Hierarquia/HierarquiaServices.ts
+++ src/Services/Hierarquia/HierarquiaServices.ts
@@ -1,12 +1,24 @@
+import prismaClient from "../../Prisma/PrismaClient";
 
     interface CadastrarHierarquia {
-    nome: String
+    nome: string
 }
 
+// nome: (coluna na base de dados) e nome (e do front end que vc vai receber)
+//dados e como uma variavel. poderia ser qualquer outro nome
+// hierarquia e o nome colocado no model e create (methodo)
+//return e o que vai aparecer no front-end após cadastrar
 class HierarquiaServices {
     async cadastrarHierarquia({nome} : CadastrarHierarquia) {
-        console.log(nome);
-    }
+        await prismaClient.hierarquia.create({
+            data: {
+                nome: nome
+            }
+        })
+        
+        return ({dados: "Cadastro Efetuado com Sucesso"})
+    } 
+    
 }
 
 
